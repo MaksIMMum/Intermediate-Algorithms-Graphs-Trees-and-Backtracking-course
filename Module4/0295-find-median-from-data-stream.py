a06bbb22c0680bd02f3ccc9f5f9e70d1ee479ba2
Maksym <72688794+MaksIMMum@users.noreply.github.com>
2026-05-06 11:29:29 +0300

    Module 4

---
+++ Module4/0295-find-median-from-data-stream.py
@@ -0,0 +1,20 @@
+class MedianFinder:
+    def __init__(self):
+        self.leftHalf = []
+        self.rightHalf = []
+    def addNum(self, num: int) -> None:
+        if not self.leftHalf or num <= -self.leftHalf[0]:
+            heapq.heappush(self.leftHalf, -num)
+        else:
+            heapq.heappush(self.rightHalf, num)
+        if len(self.leftHalf) > len(self.rightHalf) + 1:
+            heapq.heappush(self.rightHalf, -heapq.heappop(self.leftHalf))
+        elif len(self.rightHalf) > len(self.leftHalf):
+            heapq.heappush(self.leftHalf, -heapq.heappop(self.rightHalf))
+    def findMedian(self) -> float:
+        if len(self.leftHalf) > len(self.rightHalf):
+            return -self.leftHalf[0]
+        elif len(self.rightHalf) > len(self.leftHalf):
+            return self.rightHalf[0]
+        else:
+            return (-self.leftHalf[0] + self.rightHalf[0]) / 2.0
